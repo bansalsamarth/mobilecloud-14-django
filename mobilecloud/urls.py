bb4f0e66cfe6f8bcbba82b57b6f138681dc71229
samarth <bansalsamarth@yahoo.com>
2014-07-22 13:20:32 +0530

    URL updated

--- mobilecloud/urls.py
+++ mobilecloud/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from week2.views import echo_get
+from week2.views import echo_get, video_handler
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'1-SimpleServlet/echo', echo_get),
+    url(r'2-VideoServlet/video', video_handler),
 )
